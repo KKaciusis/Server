78dffe2ebae9ce1dcd8d6158e61b528ec2fe7aab
Kristijonas Kačiušis <84323029+KKaciusis@users.noreply.github.com>
2021-08-17 18:35:05 +0300

    server responce with files.

--- server.mjs
+++ server.mjs
@@ -1,22 +1,28 @@
 import * as fs from "fs/promises";
+import * as path from "path";
 import { Server } from "net";
 
 const PORT = 3000;
+const WEB = "websiteFiles";
+
 const server = new Server(socket =>{
+    socket.setEncoding("utf-8");
     let allData = "";
-    socket.on("data", data => {
+    socket.on("data", async data => {
         allData += data;
         const lines = allData.split("\r\n");
         if(lines.findIndex(l =>l === "") >= 0){
-            console.log(allData);
+            let fileName = lines[0].split(" ")[1];
             let response = "HTTP/1.1 200 OK\r\n";
             response += "Content-Type: text/html\r\n";
             response += "\r\n";
-            response += "<html><body><h1>LOBOS</h1></body></html>\r\n";
+            fileName = path.join(WEB, fileName);
+            const fileContent = await fs.readFile(fileName);
+            response += fileContent;
             response += "\r\n";
             socket.write(response, () =>{
                 socket.end();
-            })
+            });
         }
     })
 
