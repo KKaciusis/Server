dbe8e00bb47946515a3433d1fd273c9fa6fe70e4
Kristijonas Kačiušis <84323029+KKaciusis@users.noreply.github.com>
2021-08-17 16:10:13 +0300

    server responce.

--- server.mjs
+++ server.mjs
@@ -3,6 +3,22 @@ import { Server } from "net";
 
 const PORT = 3000;
 const server = new Server(socket =>{
+    let allData = "";
+    socket.on("data", data => {
+        allData += data;
+        const lines = allData.split("\r\n");
+        if(lines.findIndex(l =>l === "") >= 0){
+            console.log(allData);
+            let response = "HTTP/1.1 200 OK\r\n";
+            response += "Content-Type: text/html\r\n";
+            response += "\r\n";
+            response += "<html><body><h1>LOBOS</h1></body></html>\r\n";
+            response += "\r\n";
+            socket.write(response, () =>{
+                socket.end();
+            })
+        }
+    })
 
 });
 
