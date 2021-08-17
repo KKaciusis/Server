84f9d34f31743fdc9b8d30f1be02f02312f23074
Kristijonas Kačiušis <84323029+KKaciusis@users.noreply.github.com>
2021-08-17 15:52:32 +0300

    Create server.mjs

---
+++ server.mjs
@@ -0,0 +1,10 @@
+import * as fs from "fs/promises";
+import { Server } from "net";
+
+const PORT = 3000;
+const server = new Server(socket =>{
+
+});
+
+server.listen(PORT);
+console.log(`Server started on port: ${PORT}`);
