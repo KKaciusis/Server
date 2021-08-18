3b15af70b269916311f44636d045b7cce00920d0
Kristijonas Kačiušis <84323029+KKaciusis@users.noreply.github.com>
2021-08-18 19:42:23 +0300

    server update

--- server.mjs
+++ server.mjs
@@ -2,61 +2,105 @@ import * as fs from "fs/promises";
 import * as path from "path";
 import { Server } from "net";
 
+
 const PORT = 3000;
 const WEB = "websiteFiles";
 
-const server = new Server(socket =>{
+const server = new Server(socket => {
+
     socket.setEncoding("utf-8");
     let allData = "";
+
     socket.on("data", async data => {
         allData += data;
         const lines = allData.split("\r\n");
-        if(lines.findIndex(l =>l === "") >= 0){
+        if (lines.findIndex(l => l === "") >= 0) {
+            
             let fileName = lines[0].split(" ")[1];
-            let response = "";
-            try {
-                let fullFileName = path.join(WEB, fileName);
-                const fileContent = await fs.readFile(fullFileName);
-                response += createResponce(200, "OK", null, fileContent);
-            } catch (ERROR){
-                if(error instanceof TypeError){
-                    throw error;
-                }
-                response = createResponce( 404, "Not Found",{"Content-Type": "text/html"} `<html><body><h1>${fileName} not found</h1></body></html>` )
+            let response;
+
+            if (fileName === "/end") {
+                response = createResponse( 200, "OK", {"Content-Type": "text/html"}, `<html><body><h1>Server is going down. Bye bye.</h1></body></html>\r\n`);
+                socket.write(response, () => {
+                    socket.end(undefined, undefined, () => {
+                        server.close();
+                    });
+                });
+                return;
             }
-            socket.write(response, () =>{
+            try {
+                const fullFileName = path.join(WEB, fileName);
+                const fileStats = await fs.stat(fullFileName);
+                if (fileStats.isDirectory()) {
+                    let html = "<html>";
+                    html += "<body>";
+                    const dirFiles = await fs.readdir(fullFileName);
+                    let hrefFile = fileName;
+                    if (!hrefFile.endsWith("/")) {
+                        hrefFile += "/";
+                    }
+                    if (fileName !== "/") {
+                        html += `<a href="${hrefFile + ".."}">.. - up dir</a><br>\r\n`;
+                    }
+                    for (const dirFile of dirFiles) {
+                        html += `<a href="${hrefFile + dirFile}">${dirFile}</a><br>\r\n`;
+                    }
+                    html += "</body>";
+                    html += "</html>";
+                    response = createResponse(200,"OK",{"Content-Type": "text/html"}, html);
+                } else {
+                    const fileContent = await fs.readFile(fullFileName, {
+                        encoding: "utf-8"
+                    });
+                    response = createResponse(200, "OK", null, fileContent);
+                }
+            } catch (err) {
+                if (err instanceof TypeError) {
+                    throw err;
+                }
+                response = createResponse(
+                    404,
+                    "Not Found",
+                    {
+                        "Content-Type": "text/html"
+                    },
+                    `<html><body><h1>${fileName} not found</h1></body></html>\r\n`
+                );
+            }
+            console.log(response);
+            socket.write(response, () => {
                 socket.end();
             });
         }
-    })
-
+    });
 });
 
-function createResponce(status, msg, headers, content){
-    if (typeof status !== "number"){
-        throw new TypeError("status must be a number");
+function createResponse(status, msg, headers, content) {
+    if (typeof status !== "number") {
+        throw new TypeError("status must be number");
     }
-    if (typeof msg !== "string"){
-        throw new TypeError("msg must be a string");
+    if (typeof msg !== "string") {
+        throw new TypeError("msg must be string");
     }
-    if (headers && typeof headers !== "object"){
-        throw new TypeError("headers must be an object");
+    if (headers && typeof headers !== "object") {
+        throw new TypeError("headers must be object");
     }
-    if (content && typeof content !== "string"){
-        throw new TypeError("headers must be an object");
+    if (content && typeof content !== "string") {
+        throw new TypeError("content must be string");
     }
-    let res = `HTTP/1.1 ${status} ${msg}\r\n`
+    let res = `HTTP/1.1 ${status} ${msg}\r\n`;
     if (headers) {
-        for (const header in headers);{
-        res += `${header}: ${headers[header]}\r\n`;                
+        for (const header in headers) {
+            res += `${header}: ${headers[header]}\r\n`;
         }
-        res += "\r\n";
-        if (content){
-            res += content;
-        }
-        res += "\r\n";
-        return res;
     }
+    res += "\r\n";
+    if (content) {
+        res += content;
+    }
+    res += "\r\n";
+    return res;
 }
+
 server.listen(PORT);
 console.log(`Server started on port: ${PORT}`);
